797e2725a63ba53065602b08a46ef3e03f300f95
Davide Gironi <davide.gironi@gmail.com>
2020-05-20 21:41:59 +0200

    updated DG libraries, updated version

--- _DevTools/AutoBuilder.config.ps1
+++ _DevTools/AutoBuilder.config.ps1
@@ -6,7 +6,7 @@ $solutionName = "DentneD"
 $versionMajor = "1"
 $versionMinor = "1"
 $versionBuild = GetVersionBuild
-$versionRevision = "16"
+$versionRevision = "17"
 #build version number
 $assemblyVersion = GetVersion $versionMajor $versionMinor $versionBuild $versionRevision
 $fileVersion = $assemblyVersion
